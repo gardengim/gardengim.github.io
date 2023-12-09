83b18a8270c042efa81ae739ebcb04b6687a8a33
Garden Gim <gardengim@pm.me>
2023-12-09 03:02:06 -0800

    - remove code_highlight

--- _config.ts
+++ _config.ts
@@ -1,5 +1,4 @@
 import lume from "lume/mod.ts";
-import code_highlight from "lume/plugins/code_highlight.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
 
 const site = lume({
@@ -7,7 +6,6 @@ const site = lume({
 	location: new URL("https://gim.garden"),
 });
 
-site.use(code_highlight());
 site.use(jsx());
 
 export default site;
