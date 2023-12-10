0511971cc6130b9fd06497cf2fef6d1f902b7dca
Garden Gim <gardengim@pm.me>
2023-12-09 18:05:04 -0800

    + optimize layout

--- src/_includes/Layout.jsx
+++ src/_includes/Layout.jsx
@@ -1,27 +1,29 @@
 import Explorer from "./components/Explorer.jsx";
 import H0 from "./components/H0.jsx";
 
-export default ({ title, children, nav, url }) => {
-  return (
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="/styles.css" />
-        <title>{title}</title>
-      </head>
-      <body>
-        <nav>
-          <Explorer nav={nav} url={url} />
-        </nav>
-        <main class="border-x-2">
-          <header>{title}</header>
-          <article class="prose">
-            {children}
-          </article>
-          <footer></footer>
-        </main>
-      </body>
-    </html>
-  );
-};
+export default ({ title, children, nav, url }) => (
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="/main.css" />
+      <title>{title}</title>
+    </head>
+    <body>
+      <nav>
+        <a href="/">Garden Gim</a>
+        <Explorer nav={nav} url={url} />
+      </nav>
+      <main class="border-x-2 font-serif">
+        <header>
+          <H0>{title}</H0>
+        </header>
+        <article class="prose">
+          {children}
+        </article>
+        <footer>
+        </footer>
+      </main>
+    </body>
+  </html>
+);
