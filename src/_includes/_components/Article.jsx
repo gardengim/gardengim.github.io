6a55ec371324d1f7c5c5b98eba59854e4143cd24
Garden Gim <gardengim@pm.me>
2023-12-19 14:51:53 -0800

    + created

---
+++ src/_includes/_components/Article.jsx
@@ -0,0 +1,14 @@
+import H0 from "./H0.jsx";
+
+export default ({ children, title }) => (
+  <article class="prose dark:prose-invert prose-stone mx-auto py-8 px-4 font-serif break-words">
+    <header id="top">
+      <H0>{title}</H0>
+      <hr />
+    </header>
+    {children}
+    <footer>
+      <hr />
+    </footer>
+  </article>
+);
