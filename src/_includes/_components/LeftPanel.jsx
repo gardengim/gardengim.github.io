2fd991a134c450028afebf2f20b5f5685412ebcb
Garden Gim <gardengim@pm.me>
2023-12-31 01:39:12 +0900

    + create checkbox for toggling sub-pages

--- src/_includes/_components/LeftPanel.jsx
+++ src/_includes/_components/LeftPanel.jsx
@@ -5,6 +5,7 @@ export default ({ nav }) => {
         <li>
           <a href={child.data && child.data.url}>
             <div class="pt-2 text-stone-700 dark:text-stone-300 hover:text-stone-600 dark:hover:text-stone-400">
+              {child.children && <input type="checkbox" class="mr-2" />}
               {child.data && child.data.title || child.slug}
             </div>
           </a>
